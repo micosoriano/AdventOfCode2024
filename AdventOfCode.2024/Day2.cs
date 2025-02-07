69361483cdd2a1e05d1725bc991c0f096b993d37
Mico Soriano <micojasper.soriano@analog.com>
2025-02-07 20:45:10 +0800

    adds method to reports

--- AdventOfCode.2024/Day2.cs
+++ AdventOfCode.2024/Day2.cs
@@ -16,7 +16,6 @@
 			while (!this.reader.EndOfStream)
 			{
 				string line = reader.ReadLine();
-				//Console.WriteLine(line);
 				var intVal = Array.ConvertAll(line!.Split(" "), int.Parse).ToList();
 				List<Level> levels = new List<Level>();
 				foreach (var level in intVal)
@@ -26,39 +25,9 @@
 				reports.Add(new Report(levels));
 			}
 
-			// reports
 			foreach(var report in reports)
 			{
-				//levels
-				int prevDiff = 0;
-				int currentDiff = 0;
-
-				if (report.Levels[0].Value == 38 && report.Levels[1].Value == 41 && report.Levels[2].Value == 42)
-				{
-					var val = 0;
-				}
-				for (int i = 0; i < (report.Levels.Count - 1) && report.IsOneWay && report.IsSafe; i++)
-				{
-					prevDiff = currentDiff;
-					currentDiff = report.Levels[i].Value - report.Levels[i + 1].Value;
-					if ((prevDiff < 0 && currentDiff > 0) || (prevDiff > 0 && currentDiff < 0) || (report.Levels[i].Value == report.Levels[i + 1].Value))
-					{
-						report.IsOneWay = false;
-					}
-					else
-					{
-						report.IsOneWay = true;
-					}
-
-					if (Math.Abs(currentDiff) <= 3 && report.IsOneWay)
-					{
-						report.IsSafe = true;
-					}
-					else
-					{
-						report.IsSafe = false;
-					}
-				}
+				report.CheckIsSafe();
 
 				if (report.IsSafe && report.IsOneWay)
 				{
@@ -85,6 +54,40 @@
 			this.Levels = levels;
 			this.IsSafe = true;
 			this.IsOneWay = true;
+		}
+
+		public void CheckIsSafe()
+		{
+			//levels
+			int prevDiff = 0;
+			int currentDiff = 0;
+
+			if (this.Levels[0].Value == 38 && this.Levels[1].Value == 41 && this.Levels[2].Value == 42)
+			{
+				var val = 0;
+			}
+			for (int i = 0; i < (this.Levels.Count - 1) && this.IsOneWay && this.IsSafe; i++)
+			{
+				prevDiff = currentDiff;
+				currentDiff = this.Levels[i].Value - this.Levels[i + 1].Value;
+				if ((prevDiff < 0 && currentDiff > 0) || (prevDiff > 0 && currentDiff < 0) || (this.Levels[i].Value == this.Levels[i + 1].Value))
+				{
+					this.IsOneWay = false;
+				}
+				else
+				{
+					this.IsOneWay = true;
+				}
+
+				if (Math.Abs(currentDiff) <= 3 && this.IsOneWay)
+				{
+					this.IsSafe = true;
+				}
+				else
+				{
+					this.IsSafe = false;
+				}
+			}
 		}
 	}
 
