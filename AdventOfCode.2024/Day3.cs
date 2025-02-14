977154f8de41701c48e043ae59fa086645fc6b5f
Mico Soriano <micojasper.soriano@analog.com>
2025-02-14 11:25:37 +0800

    checkpoint

--- AdventOfCode.2024/Day3.cs
+++ AdventOfCode.2024/Day3.cs
@@ -12,27 +12,83 @@
 
 		public void Task1()
 		{
-			List<string> operations = new List<string>();
 			int mulResult = 0;
+			List<MulOperation> mulOperations = new List<MulOperation>();
 			while (!this.reader.EndOfStream)
 			{
+				List<DoOperation> doOperations = new List<DoOperation>();
+				List<DontOperation> dontOperations = new List<DontOperation>();
 				string pattern = @"mul\(\d+,\d+\)";
+				string doPattern = "do()";
+				string dontPattern = "don't()";
 				string line = reader.ReadLine()!;
 				var matches = Regex.Matches(line, pattern);
+				var doMatches = Regex.Matches(line, doPattern);
+				var dontMatches = Regex.Matches(line, dontPattern);
 				foreach (Match match in matches)
 				{
-					operations.Add(match.Value);
 					Console.WriteLine(match.Value);
 					var digits = Regex.Matches(match.Value, @"\d+");
-					mulResult = mulResult + (int.Parse(digits[0].Value) * int.Parse(digits[1].Value));
-					Console.WriteLine("Result: " + mulResult);
+					mulOperations.Add(new MulOperation(int.Parse(digits[0].Value), int.Parse(digits[1].Value), match.Index));
 				}
+
+				foreach (Match match in doMatches)
+				{
+					Console.WriteLine(match.Value);
+					Console.WriteLine(match.Index);
+					doOperations.Add(new DoOperation(match.Index));
+				}
+
+				foreach (Match match in dontMatches)
+				{
+					Console.WriteLine(match.Value);
+					Console.WriteLine(match.Index);
+					dontOperations.Add(new DontOperation(match.Index));
+				}
+
+
 			}
 		}
 
 		public void Task2()
 		{
 
+		}
+
+		class MulOperation
+		{
+			public int Operand1 { get; set; }
+			public int Operand2 { get; set; }
+			public int Position { get; set; }
+			public bool IsEnabled { get; set; }
+
+			public MulOperation(int operand1, int operand2, int position)
+			{
+				Operand1 = operand1;
+				Operand2 = operand2;
+				Position = position;
+				IsEnabled = true;
+			}
+		}
+
+		class DoOperation
+		{
+			public int Position { get; set; }
+
+			public DoOperation(int position)
+			{
+				Position = position;
+			}
+		}
+
+		class DontOperation
+		{
+			public int Position { get; set; }
+
+			public DontOperation(int position)
+			{
+				Position = position;
+			}
 		}
 	}
 }
