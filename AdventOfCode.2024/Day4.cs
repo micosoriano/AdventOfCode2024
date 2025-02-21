dae3cddc2234afc0caea0f9ad77ae393280c5476
Mico Soriano <micojasper.soriano@analog.com>
2025-02-21 18:21:55 +0800

    improves readability

--- AdventOfCode.2024/Day4.cs
+++ AdventOfCode.2024/Day4.cs
@@ -5,14 +5,14 @@
 
 	internal class Day4 : Day, IDay
 	{
-		string xPattern = @"X";
-		string mPattern = @"M";
-		string aPattern = @"A";
-		string sPattern = @"S";
-		List<XMAS> xList = new List<XMAS>();
-		List<XMAS> mList = new List<XMAS>();
-		List<XMAS> aList = new List<XMAS>();
-		List<XMAS> sList = new List<XMAS>();
+		readonly string xPattern = @"X";
+		readonly string mPattern = @"M";
+		readonly string aPattern = @"A";
+		readonly string sPattern = @"S";
+		readonly List<XMAS> xList = new List<XMAS>();
+		readonly List<XMAS> mList = new List<XMAS>();
+		readonly List<XMAS> aList = new List<XMAS>();
+		readonly List<XMAS> sList = new List<XMAS>();
 
 		public Day4(string input) : base(input)
 		{
@@ -21,34 +21,16 @@
 			while (!this.reader.EndOfStream)
 			{
 				string line = reader.ReadLine()!;
+
 				var xMatches = Regex.Matches(line, xPattern);
 				var mMatches = Regex.Matches(line, mPattern);
 				var aMatches = Regex.Matches(line, aPattern);
 				var sMatches = Regex.Matches(line, sPattern);
 
-				foreach (Match match in xMatches)
-				{
-					X x = new X(match.Index, y);
-					xList.Add(x);
-				}
-
-				foreach (Match match in mMatches)
-				{
-					M m = new M(match.Index, y);
-					mList.Add(m);
-				}
-
-				foreach (Match match in aMatches)
-				{
-					A a = new A(match.Index, y);
-					aList.Add(a);
-				}
-
-				foreach (Match match in sMatches)
-				{
-					S s = new S(match.Index, y);
-					sList.Add(s);
-				}
+				foreach (Match match in xMatches) xList.Add(new X(match.Index, y));
+				foreach (Match match in mMatches) mList.Add(new M(match.Index, y));
+				foreach (Match match in aMatches) aList.Add(new A(match.Index, y));
+				foreach (Match match in sMatches) sList.Add(new S(match.Index, y));
 
 				y++;
 			}
@@ -57,7 +39,6 @@
 			mList.OrderBy(x => x.YLoc).ThenBy(x => x.XLoc);
 			aList.OrderBy(x => x.YLoc).ThenBy(x => x.XLoc);
 			sList.OrderBy(x => x.YLoc).ThenBy(x => x.XLoc);
-
 		}
 
 		public void Task1()
@@ -68,21 +49,9 @@
 				for (int i = 0; i < 8; i++)
 				{
 					XMAS next = x.FindNext(mList, (Direction)i);
-
-					if (next != null)
-					{
-						next = next.FindNext(aList, (Direction)i);
-					}
-
-					if (next != null)
-					{
-						next = next.FindNext(sList, (Direction)i);
-					}
-
-					if (next != null)
-					{
-						xmasCount++;
-					}
+					if (next != null) next = next.FindNext(aList, (Direction)i);
+					if (next != null) next = next.FindNext(sList, (Direction)i);
+					if (next != null) xmasCount++;
 				}
 			}
 
@@ -92,66 +61,33 @@
 		public void Task2()
 		{
 			int xmasCount = 0;
-
 			foreach (var a in aList)
 			{
 				var xmas = 0;
 
 				// s on the north east
 				var NES = a.FindNext(sList, Direction.NE);
-				XMAS SWM = null;
-
-				if (NES != null)
-				{
-					SWM = a.FindNext(mList, Direction.SW);
-				}
-
-				if (SWM != null)
-				{
-					xmas++;
-				}
+				XMAS SWM = null!;
+				if (NES != null) SWM = a.FindNext(mList, Direction.SW);
+				if (SWM != null) xmas++;
 				
 				// m on the north east
 				var NEM = a.FindNext(mList, Direction.NE);
-				XMAS SWS = null;
-
-				if (NEM != null)
-				{
-					SWS = a.FindNext(sList, Direction.SW);
-				}
-
-				if (SWS != null)
-				{
-					xmas++;
-				}
+				XMAS SWS = null!;
+				if (NEM != null) SWS = a.FindNext(sList, Direction.SW);
+				if (SWS != null) xmas++;
 
 				// s on the south east
 				var SES = a.FindNext(sList, Direction.SE);
-				XMAS NWM = null;
-
-				if (SES != null)
-				{
-					NWM = a.FindNext(mList, Direction.NW);
-				}
-
-				if (NWM != null)
-				{
-					xmas++;
-				}
+				XMAS NWM = null!;
+				if (SES != null) NWM = a.FindNext(mList, Direction.NW);
+				if (NWM != null) xmas++;
 
 				// m on the south east
 				var SEM = a.FindNext(mList, Direction.SE);
-				XMAS NWS = null;
-
-				if (SEM != null)
-				{
-					NWS = a.FindNext(sList, Direction.NW);
-				}
-
-				if (NWS != null)
-				{
-					xmas++;
-				}
+				XMAS NWS = null!;
+				if (SEM != null) NWS = a.FindNext(sList, Direction.NW);
+				if (NWS != null) xmas++;
 
 				if (xmas == 2) xmasCount++;
 			}
@@ -203,6 +139,8 @@
 						return FindSouthEast(list);
 					case Direction.SW:
 						return FindSouthWest(list);
+					default:
+						break;
 				}
 
 				return null!;
