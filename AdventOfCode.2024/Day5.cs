b7dea2d9ecb2e96cc494399a34c894bdac89daf1
Mico Soriano <micojasper.soriano@analog.com>
2025-03-07 11:07:52 +0800

    working addition of pages

--- AdventOfCode.2024/Day5.cs
+++ AdventOfCode.2024/Day5.cs
@@ -14,15 +14,24 @@
 			Console.WriteLine("Advent of Code Day 5");
 
 			List<Page> pages = new List<Page>();
+			PageHandler pageHandler = new PageHandler();
 			do
 			{
 				var line = reader.ReadLine()!;
 				if (line == "") break;
-				pages.Add(new Page { Value = int.Parse(line.Split("|")[0]) });
-				pages.Add(new Page { Value = int.Parse(line.Split("|")[1]) });
+				var split = Array.ConvertAll(line.Split("|"), int.Parse);
+				pageHandler.AddLeft(new Page(split[0]));
+				pageHandler.AddRight(new Page(split[1]));
 				Console.WriteLine(line);
 			}
 			while (true);
+
+			while (!this.reader.EndOfStream)
+			{
+				string line = reader.ReadLine()!;
+				var split = Array.ConvertAll(line.Split(","), int.Parse);
+
+			}
 		}
 
 		public void Task1()
@@ -38,7 +47,41 @@
 
 	class Page
 	{
-		public int Value { get; set; }
-		public int Rank { get; set; }
+		public int Value { get; }
+		public int Rank { get; set; } = 0;
+		public Page(int val)
+		{
+			this.Value = val;
+		}
+	}
+
+	class PageHandler
+	{
+		List<Page> pages = new List<Page>();
+
+		public PageHandler()
+		{
+			
+		}
+
+		public void AddLeft(Page page)
+		{
+			var left = pages.FirstOrDefault(p => p.Value == page.Value);
+			if (left != null)
+			{
+				left.Rank += 1;
+			}
+			else
+			{
+				page.Rank += 1;
+				pages.Add(page);
+			}
+		}
+
+		public void AddRight(Page page)
+		{
+			var right = pages.FirstOrDefault(p => p.Value == page.Value);
+			if (right == null) pages.Add(page);
+		}
 	}
 }
