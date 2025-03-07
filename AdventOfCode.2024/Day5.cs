55750ac859a3c889eb240995d1f65d45bbe26b6e
Mico Soriano <micojasper.soriano@analog.com>
2025-03-07 14:26:15 +0800

    working task 1

--- AdventOfCode.2024/Day5.cs
+++ AdventOfCode.2024/Day5.cs
@@ -13,15 +13,14 @@
 		{
 			Console.WriteLine("Advent of Code Day 5");
 
-			List<Page> pages = new List<Page>();
 			PageHandler pageHandler = new PageHandler();
 			int sum = 0;
 			do {
 				var line = reader.ReadLine()!;
 				if (line == "") break;
 				var split = Array.ConvertAll(line.Split("|"), int.Parse);
-				pageHandler.AddLeft(new Page(split[0]));
-				pageHandler.AddRight(new Page(split[1]));
+
+				pageHandler.AddPages(new Page(split[0]), new Page(split[1]));
 				Console.WriteLine(line);
 			}
 			while (true);
@@ -48,65 +47,64 @@
 
 	class Page
 	{
+		public List<int> OnRightOf { get; set; }
+		public List<int> OnLeftOf { get; set; }
 		public int Value { get; }
-		public int Rank { get; set; }
 		public Page(int val)
 		{
 			this.Value = val;
-			this.Rank = 100;
+			OnRightOf = new List<int>();
+			OnLeftOf = new List<int>();
 		}
 	}
 
 	class PageHandler
 	{
 		List<Page> pages;
-		public List<Page> Pages { get => pages.OrderBy(p => p.Rank).ToList(); }
+		public List<Page> Pages { get => pages; set => pages = value; }
 
 		public PageHandler()
 		{
 			pages = new List<Page>();
 		}
 
-		public void AddLeft(Page page)
+		public void AddPages(Page onLeft, Page onRight)
 		{
-			var left = pages.FirstOrDefault(p => p.Value == page.Value);
-			if (left != null)
+			var left = pages.FirstOrDefault(p => p.Value == onLeft.Value);
+			var right = pages.FirstOrDefault(p => p.Value == onRight.Value);
+			if (left == null)
 			{
-				left.Rank += 1;
+				left = onLeft;
+				pages.Add(onLeft);
 			}
-			else
+			if (right == null)
 			{
-				page.Rank += 1;
-				pages.Add(page);
+				right = onRight;
+				pages.Add(right);
 			}
-		}
-
-		public void AddRight(Page page)
-		{
-			var right = pages.FirstOrDefault(p => p.Value == page.Value);
-			if (right != null)
-			{
-				right.Rank -= 1;
-			}
-			else
-			{
-				page.Rank -= 1;
-				pages.Add(page);
-			}
+			left.OnLeftOf.Add(right.Value);
+			right.OnRightOf.Add(left.Value);
 		}
 
 		public bool IsInOrder(List<int> lstPage)
 		{
 			bool isInOrder = false;
-			for (int i = 0; i < (lstPage.Count - 1); i++) {
-				var msb = pages.FirstOrDefault(p => p.Value == lstPage[i]);
-				var lsb = pages.FirstOrDefault(p => p.Value == lstPage[i + 1]);
-				if (msb?.Rank <= lsb?.Rank) {
-					Console.WriteLine("Not in order");
-					isInOrder = false;
-					break;
+			for (int i = 0; i < (lstPage.Count); i++)
+			{
+				var onLeft = pages.FirstOrDefault(p => p.Value == lstPage[i]);
+
+				for (int j = i + 1; j < (lstPage.Count); j++)
+				{
+					var onRight = pages.FirstOrDefault(p => p.Value == lstPage[j]);
+					if (!onLeft!.OnLeftOf.Contains(onRight!.Value) ||
+						!onRight!.OnRightOf.Contains(onLeft!.Value))
+					{
+						isInOrder = false;
+						break;
+					}
+					else isInOrder = true;
 				}
-				else isInOrder = true;
+				if (!isInOrder) break;
 			}
 			return isInOrder;
 		}
