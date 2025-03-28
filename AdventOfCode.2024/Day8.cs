4fc1a54073e4eefdd22437595ab5e2e8ea50b5fc
Mico Soriano <micojasper.soriano@analog.com>
2025-03-28 14:20:15 +0800

    day8

---
+++ AdventOfCode.2024/Day8.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode
+{
+    using AdventOfCode.Helpers;
+
+    internal class Day8 : Day
+    {
+        public Day8(string input) : base(input)
+        {
+            Console.WriteLine("Advent of Code Day 8");
+        }
+    }
+}
