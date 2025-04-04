0942924252d7c3442bf39c8ae2c7e5b2e3e3c541
Mico Soriano <micojasper.soriano@analog.com>
2025-04-04 10:58:51 +0800

    nakapagparse din!

--- AdventOfCode.2024/Day9.cs
+++ AdventOfCode.2024/Day9.cs
@@ -1,12 +1,19 @@
 ﻿namespace AdventOfCode
 {
+    using System.Text.RegularExpressions;
     using AdventOfCode.Helpers;
 
     internal class Day9 : Day
     {
+        List<int> fileSystem;
         public Day9(string input) : base(input)
         {
+            fileSystem = new List<int>();
             Console.WriteLine("Advent of Code Day 9");
+            foreach(var file in reader.ReadLine()!)
+            {
+                fileSystem.Add(int.Parse(file.ToString()));
+            }
         }
     }
 }
