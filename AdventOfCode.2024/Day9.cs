7b51b2561ea3dd453e866faac4b3c80d949bb38b
Mico Soriano <micojasper.soriano@analog.com>
2025-04-04 11:53:20 +0800

    working part1

--- AdventOfCode.2024/Day9.cs
+++ AdventOfCode.2024/Day9.cs
@@ -5,15 +5,58 @@
 
     internal class Day9 : Day
     {
-        List<int> fileSystem;
+        List<int> rawData;
+        List<string> fileSystem;
         public Day9(string input) : base(input)
         {
-            fileSystem = new List<int>();
+            rawData = new List<int>();
+            fileSystem = new List<string>();
             Console.WriteLine("Advent of Code Day 9");
-            foreach(var file in reader.ReadLine()!)
+            var line = reader.ReadLine()!;
+            int y = 0;
+            foreach (var file in line)
             {
-                fileSystem.Add(int.Parse(file.ToString()));
+                string fileString = file.ToString();
+                for (int i = 0; i < int.Parse(fileString); i++)
+                {
+                    if (y % 2 == 0)
+                    {
+                        fileSystem.Add((y/2).ToString());
+                    }
+                    else
+                    {
+                        fileSystem.Add(".");
+                    }
+                }
+                y++;
             }
+        }
+
+        public void Task1()
+        {
+            for (int i = fileSystem.Count - 1; i > 0; i--)
+            {
+                var idxSpace = fileSystem.IndexOf(".");
+                var checker = fileSystem.Skip(idxSpace);
+                if (!checker.Any(s => int.TryParse(s, out _))) break;
+                
+                if (fileSystem[i] != ".")
+                {
+                    fileSystem[idxSpace] = fileSystem[i];
+                    fileSystem[i] = ".";
+                }
+            }
+
+            double sum = 0;
+            for (int i = 0; i < fileSystem.Count; i++)
+            {
+                if (int.TryParse(fileSystem[i], out var val))
+                {
+                    sum += val*i;
+                }
+            }
+
+            Console.WriteLine("Sum: " + sum);
         }
     }
 }
