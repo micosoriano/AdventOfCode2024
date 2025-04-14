5dd9a903b727f6987fb66bb8f70685d440ef9518
Mico Soriano <micojasper.soriano@analog.com>
2025-04-14 11:25:06 +0800

    tries to optimize using dict

--- AdventOfCode.2024/Day11.cs
+++ AdventOfCode.2024/Day11.cs
@@ -6,10 +6,12 @@
     internal class Day11 : Day
     {
         List<string> stones;
+        Dictionary<string, List<string>> knownStones;
         public Day11(string input) : base(input)
         {
             Console.WriteLine("Advent of Code Day 11");
             stones = new List<string>();
+            knownStones = new Dictionary<string, List<string>>();
             stones.AddRange(reader.ReadLine()!.Split(" "));
 
         }
@@ -17,7 +19,7 @@
         public void Task1()
         {
             Blink(75);
-            Console.WriteLine("Stones: " + string.Join(", ", stones));
+            //Console.WriteLine("Stones: " + string.Join(", ", stones));
             Console.WriteLine("Count: " + stones.Count);
         }
 
@@ -28,29 +30,36 @@
             while (curBlink <= blinks)
             {
                 tempStones.Clear();
-                Parallel.ForEach(stones, stone =>
+                foreach (var stone in stones)
                 {
                     List<string> tempStone = new List<string>();
 
-                    if (stone == "0") tempStone.Add("1");
-                    else if (stone.Length % 2 == 0)
+                    if (knownStones.ContainsKey(stone))
                     {
-                        var list = stone.ToCharArray().ToList();
-                        var firstHalf = list.Take(list.Count / 2).ToList();
-                        var secondHalf = list.Skip(list.Count / 2).ToList();
-                        tempStone.Add(new string(firstHalf.ToArray()));
-                        tempStone.Add(double.Parse(secondHalf.ToArray()).ToString());
+                        tempStone = knownStones[stone];
                     }
                     else
                     {
-                        var doubleVal = double.Parse(stone);
-                        tempStone.Add((doubleVal * 2024).ToString());
+                        if (stone == "0") tempStone.Add("1");
+                        else if (stone.Length % 2 == 0)
+                        {
+                            var list = stone.ToCharArray().ToList();
+                            var firstHalf = list.Take(list.Count / 2).ToList();
+                            var secondHalf = list.Skip(list.Count / 2).ToList();
+                            tempStone.Add(new string(firstHalf.ToArray()));
+                            tempStone.Add(double.Parse(secondHalf.ToArray()).ToString());
+                        }
+                        else
+                        {
+                            var doubleVal = double.Parse(stone);
+                            tempStone.Add((doubleVal * 2024).ToString());
+                        }
+                        knownStones.Add(stone, tempStone);
+
                     }
-                    lock (tempStones)
-                    {
-                        tempStones.AddRange(tempStone);
-                    }
-                });
+                    tempStones.AddRange(tempStone);
+
+                }
                 stones = new List<string>(tempStones);
                 //Console.WriteLine($"Current Stones at blink {curBlink}: " + string.Join(", ", tempStones));
                 Console.WriteLine("Current blink " + curBlink);
