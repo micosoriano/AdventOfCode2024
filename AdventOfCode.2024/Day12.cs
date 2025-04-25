dd199871d8061563a3ddfe1d6eec320c3846ae04
Mico Soriano <micojasper.soriano@analog.com>
2025-04-25 10:56:36 +0800

    adds functionality to get perimeter

--- AdventOfCode.2024/Day12.cs
+++ AdventOfCode.2024/Day12.cs
@@ -22,6 +22,11 @@
                 y++;
             }
         }
+
+        public void Task1()
+        {
+
+        }
     }
 
     class Plant
@@ -32,6 +37,38 @@
         {
             this.Value = value;
             this.Position = position;
+        }
+
+        public int GetPerimeter(List<Plant> garden)
+        {
+            int perimeter = 4;
+
+            if (FindNorth(garden)) perimeter--;
+            if (FindSouth(garden)) perimeter--;
+            if (FindEast(garden)) perimeter--;
+            if (FindWest(garden)) perimeter--;
+
+            return perimeter;
+        }
+
+        public bool FindNorth(List<Plant> garden)
+        {
+            return garden.Find(x => x.Position == new Point(this.Position.X, this.Position.Y - 1) && x.Value == this.Value)! != null;
+        }
+
+        public bool FindSouth(List<Plant> garden)
+        {
+            return garden.Find(x => x.Position == new Point(this.Position.X, this.Position.Y + 1) && x.Value == this.Value)! != null;
+        }
+
+        public bool FindEast(List<Plant> garden)
+        {
+            return garden.Find(x => x.Position == new Point(this.Position.X + 1, this.Position.Y) && x.Value == this.Value)! != null;
+        }
+
+        public bool FindWest(List<Plant> garden)
+        {
+            return garden.Find(x => x.Position == new Point(this.Position.X - 1, this.Position.Y) && x.Value == this.Value)! != null;
         }
     }
 }
